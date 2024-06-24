e53545700698723ce53fd1c7e67f4d067392960a
Lisha Marathe <144786948+Coderlisha@users.noreply.github.com>
2024-06-25 00:35:16 +0530

    Add files via upload

---
+++ compa.py
@@ -0,0 +1,12 @@
+a=5
+b=4
+print(a==b) #false
+c=5
+d=5
+print(5==5)
+x=7 
+y=8
+print(x>=y)
+print(b<=y)
+print(x!=a)
+print(x//a)
---
+++ control.py
@@ -0,0 +1,13 @@
+# #control statement
+# a=1
+# b=4
+# c=9
+# if a>b and b<c :
+#     print("TRUE")
+# elif a>b and a==c:
+#     print("TRUE2")
+# else :
+#     print("FALSE") 
+a = int (input('Enter and no.'))
+print(type(a))
+print("my age is",a)
