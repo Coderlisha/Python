306ca33a8178a122dba29c99970012ce2ef89543
Lisha Marathe <144786948+Coderlisha@users.noreply.github.com>
2024-06-29 13:29:14 +0530

    Add files via upload

---
+++ divisible5and6.py
@@ -0,0 +1,6 @@
+a = int (input('Enter and no.'))
+b = int (input('Enter and no.'))
+if a%5==0 and b%6==0 :
+    print("yes no is divisiable")
+else :
+    print("No divisiable")    
---
+++ firstprogram.py
@@ -0,0 +1,2 @@
+print("Hello Python")
+print("Prasannajeet Devendra Jain")
