50095697c1a29d5179c8730bb893620c4901b6e8
Lisha Marathe <144786948+Coderlisha@users.noreply.github.com>
2024-06-30 22:23:56 +0530

    Add files via upload

---
+++ forandwhile.py
@@ -0,0 +1,8 @@
+x=[1,2,3,4,5,6]
+for i in range(0,100,2) :
+    print(i)
+############
+a=1
+while a!=5:
+    print(a)
+    a +=1
---
+++ function.py
@@ -0,0 +1,27 @@
+
+def fun1():
+    print("Sunday")
+def fun2():
+    print("Monday")
+def fun3():
+    print("Tuesday")
+def fun4():
+    print("Wednesday")
+def fun5():
+    print("Thursday")
+def fun5():
+    print("Friday")
+def fun6():
+    print("Saturday")
+fun1()
+fun3()
+fun6()    
+
+def func1(a=0,b=0):
+ print(a+b)
+func1(35,5)
+
+def func2(a=0,b=0):
+ return (a*a+2*a*b+b*b) 
+print (func2(10,10))
+ 
