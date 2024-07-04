90c951318f8df49ef4ee69336f997dc26c29fe6a
Lisha Marathe <144786948+Coderlisha@users.noreply.github.com>
2024-07-04 09:59:59 +0530

    Add files via upload

---
+++ largest.py
@@ -0,0 +1,6 @@
+a=2
+b=3
+if a>b :
+    print("a is largest")
+else :
+    print("b is largest")    
---
+++ largest3.py
@@ -0,0 +1,9 @@
+a =2
+b=3
+c=4
+if a>b and b>c :
+    print("a is largest")
+elif b>a and a>c:
+    print("b is largest")
+else :
+    print("c is largest")    
