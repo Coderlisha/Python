bb4c3c86c2e37b28e3dd8247d3dcd0b49298eed8
Lisha Marathe <144786948+Coderlisha@users.noreply.github.com>
2024-07-11 15:17:33 +0530

    Add files via upload

---
+++ leapyear.py
@@ -0,0 +1,5 @@
+a=2015
+if a%4==0 or a%100==0 or a%400==0 :
+    print("Leap Year")
+else :
+    print("Not a Leap year")    
---
+++ length.py
@@ -0,0 +1,4 @@
+thislist=["ramesh","suresh","5star","perk","sunfeast","parle","mango"]
+print(len(thislist))
+thislist.insert(2,"Brother")
+print(len(thislist))
